255a1af7778beaee5444a6ae9a5b8e0d92df967b
Hrusikesh Swain <hemantaswn@gmail.com>
2025-10-14 01:48:57 +0530

    android upload try again 1

--- .github/scripts/upload-firebase-ios.sh
+++ .github/scripts/upload-firebase-ios.sh
@@ -4,4 +4,3 @@ firebase appdistribution:distribute CICDTestApp.ipa \
   --app "$IOS_APP_ID" \
   --token "$FIREBASE_TOKEN" \
   --release-notes "Simulator build only - not installable on devices"
-  # --groups testers
